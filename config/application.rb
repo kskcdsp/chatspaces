90d0bae9f35ec04c4f34171f7965def2ad6e9c55
浅井天海 <ksk.cdsp@gmail.com>
2018-04-08 14:57:23 +0900

    created devise

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ Bundler.require(*Rails.groups)
 
 module Chatspace
   class Application < Rails::Application
+    config.i18n.default_locale = :ja
     config.generators do |g|
         g.test_framework false
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :messages
-  resources :users
+  resources :users, only: [:edit, :update, :destroy]
   root 'messages#index'
 end
