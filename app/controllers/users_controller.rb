08115a48bafde981515c7f513690cf5e6a48d163
浅井天海 <ksk.cdsp@gmail.com>
2018-04-14 20:16:10 +0900

    Correction files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
-  def edit
-  end
+  def edit; end
 
   def update
     if current_user.update(user_params)
@@ -11,6 +10,7 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user_params
     params.require(:user).permit(:name, :email)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :messages
   resources :users, only: [:edit, :update, :destroy]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
   root 'messages#index'
 end
